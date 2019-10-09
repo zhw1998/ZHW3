99ffa6388cf900c3927ebaa3287373a53f2e32b3
zhw1998 <48516498+zhw1998@users.noreply.github.com>
2019-10-10 00:22:31 +0800

    Update FileUploadConfig.java

--- MyWp/src/main/java/com/zhw/utils/FileUploadConfig.java
+++ MyWp/src/main/java/com/zhw/utils/FileUploadConfig.java
@@ -1,6 +1,8 @@
 package com.zhw.utils;
 
 public  class FileUploadConfig {
+    //文件实际上传路径
     public final static String FILE_UPLOAD_PATH = "D://WPUpload/";
+    //服务器中（tomcat）配置的虚拟路径
     public final static String WEB_PATH = "/files/";
 }
